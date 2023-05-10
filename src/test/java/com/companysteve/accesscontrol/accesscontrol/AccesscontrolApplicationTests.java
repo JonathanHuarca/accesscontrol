d05573af8e75ea15f68fa5832c1c2a615568575d
Jonathan Steve <jhuarca.peruapps@gmail.com>
2023-05-09 22:03:09 -0500

    add: first commit

---
+++ src/test/java/com/companysteve/accesscontrol/accesscontrol/AccesscontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.companysteve.accesscontrol.accesscontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccesscontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
